f9a8265a220c0664a624bf3b2567a5096049ae33
Iggykoop4 <ivossotski@gmail.com>
2024-01-22 13:42:38 +0200

    mod2 tehtävät

--- Moduuli02/tehtävä2.py
+++ Moduuli02/tehtävä2.py
@@ -1,5 +1,10 @@
 # Kirjoita ohjelma, joka kysyy ympyrän säteen ja tulostaa sen pinta-alan.
-radius = float(input("What's your circle radius?\n"))
-pii = 3.141592653589793
-new_area = ((pii) * (radius) * (radius))
-print("Your circle Area is: ", new_area)
+#radius = float(input("What's your circle radius?\n"))
+#pii = 3.141592653589793
+#new_area = ((pii) * (radius) * (radius))
+#print("Your circle Area is: ", new_area)
+
+import math
+radius = float(input("Your radius?"))
+new_area = (math.pi * radius**2)
+print(f"The area of your circle is:\n{new_area:.2f}")
