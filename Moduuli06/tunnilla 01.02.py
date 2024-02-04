01e74c0e4ab3f6c5ec331e74b13098848fef7d39
Iggykoop4 <156555346+Iggykoop4@users.noreply.github.com>
2024-02-04 18:30:24 +0200

    asd

---
+++ Moduuli06/tunnilla 01.02.py
@@ -0,0 +1,22 @@
+"""thelist = ["John", 1, 5, 3, 1, "John", True, "John"]
+newlist = []
+
+for i in thelist:
+    if i not in newlist:
+        newlist.append(i)
+    elif not in newlist:
+        newlist.append()
+print(newlist)
+"""
+"""lista = [1, 2, 3, 5]
+lista2 = [4, [6, 8], 9]
+lista.extend(lista2)
+print(lista)
+print(lista[5][1])"""
+
+"""lista = ["John", 1, 5, 3, 1, "John"]
+#setti = set(lista)
+#print(setti)
+#munlista = list(setti)
+#print(munlista)
+print(list(set(lista)))"""
